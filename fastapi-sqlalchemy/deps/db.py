77ba361d824d97f7213acca4700ecbf1ff8d14be
uuip <uuip@users.noreply.github.com>
2024-11-27 21:44:22 +0800

    update dependencies

--- fastapi-sqlalchemy/deps/db.py
+++ fastapi-sqlalchemy/deps/db.py
@@ -7,7 +7,7 @@ from config import settings
 
 url = urlparse(settings.db)._replace(scheme="postgresql+asyncpg").geturl()
 async_db = create_async_engine(url, echo=False, pool_size=50)
-async_session_factory = async_sessionmaker(bind=async_db)
+async_session_factory = async_sessionmaker(bind=async_db, expire_on_commit=False)
 
 
 async def async_session():
